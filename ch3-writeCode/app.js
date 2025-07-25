9026d8c3960b75a297317fdf126a4926aff97d86
vinit <vinitkumar93341@gmail.com>
2025-07-25 16:42:59 +0530

    let's write code

---
+++ ch3-writeCode/app.js
@@ -0,0 +1,19 @@
+let name = "vinit kumar";
+
+let a = 10;
+let b  = 20;
+
+let c = a+b; 
+
+console.table(name);
+console.log(c);
+
+console.log("code is executed line-line by v8 engine");
+
+// console.log(global);
+
+// console.log(this);
+
+// console.log(globalThis);
+
+console.log(globalThis === global); // true
